d0f67511d278f58aaca89ea470acfda0fdfe2522
Lildark21 <lilianorrit@gmail.com>
2023-12-07 10:08:22 +0100

    Main

---
+++ battle-game/main.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("../")
+
+from gear.weapon import Weapon
+# from gear.armor import Armor
+
+from characters.barbarian import Barbarian
+from characters.wizard import Wizard
+
+
+arme = Weapon("Hache a 2 mains")
+# defense = Defense("")
+
+print(arme.nom)
+print(defense.nom)
