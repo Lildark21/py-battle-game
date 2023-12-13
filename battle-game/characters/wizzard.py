8d489fa85a55748d5cb196218f196fa3cf683f3d
Skyzonox <enzo.eniona@gmail.com>
2023-12-13 17:22:48 +0100

    wizzard
    test

--- battle-game/characters/wizzard.py
+++ battle-game/characters/wizzard.py
@@ -1,3 +1,7 @@
+from gear.armure import Armor
+from characters.barbarian import Barbarian
+
+
 class Wizzard:
     def __init__(self,name,hp,weapon,mana,level):
         self.name = name
@@ -12,6 +16,16 @@ class Wizzard:
         print('weapon:',self.weapon)
         print('mana:',self.mana)
         print('level',self.level)
+ 
+ 
+    def attackwizzard(self,damage):
+        Barbarian.health - self.weapon
+        print(Barbarian.health)
+        
+
+   
+    
+    
 
     
     
